e7a7c6f2c1df63a8f758e740c060036bd885796b
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2023-05-19 23:25:35 +0900

    OAuth2サーバの雛形作成

--- oauth2/app/Main.hs
+++ oauth2/app/Main.hs
@@ -1,6 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai as Wai
+import qualified Network.HTTP.Types as HTypes
 
 main :: IO ()
-main = someFunc
+main = Warp.run 8080 router
+
+router :: Wai.Application
+router req = 
+    case Wai.pathInfo req of
+        ["auth"]        -> auth req
+        ["authcheck"]   -> authcheck req
+        ["token"]       -> token req
+        _               -> notFound req
+
+auth :: Wai.Application
+auth req send = send $ Wai.responseBuilder HTypes.status200 [] ""
+
+authcheck :: Wai.Application
+authcheck req send = send $ Wai.responseBuilder HTypes.status200 [] ""
+
+token :: Wai.Application
+token req send = send $ Wai.responseBuilder HTypes.status200 [] ""
+
+notFound :: Wai.Application
+notFound req send = send $ Wai.responseBuilder HTypes.status404 [] "Not Found"
