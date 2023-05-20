b4bae50662d96cf3ab043b692203cc23bb7bd437
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2023-05-20 16:29:43 +0900

    app

--- sso/app/app/Main.hs
+++ sso/app/app/Main.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Lib
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai as Wai 
+import qualified Network.HTTP.Types as HTypes
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn $ "Running on http://localhost:" ++ show port
+    Warp.run port router
+    where
+        port = 8080
+
+router :: Wai.Application
+router req =
+    case Wai.pathInfo req of
+        []        -> index req
+        ["login"] -> login req
+        _         -> notFound req
+
+index :: Wai.Application
+index req send = send $ Wai.responseBuilder HTypes.status200 [] "app"
+
+login :: Wai.Application
+login req send = send $ Wai.responseBuilder HTypes.status200 [] "login"
+
+notFound :: Wai.Application
+notFound req send = send $ Wai.responseBuilder HTypes.status404 [] "Not Found"
